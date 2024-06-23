d4688aa1a09a70293bfcc7113e4f8c54893edc56
abdulaziz <abdulaziz@gmail.com>
2024-06-23 21:22:57 +0500

    git add.

---
+++ my.js
@@ -0,0 +1,23 @@
+let styll=document.querySelector('.styll')
+let sty=document.querySelector('.sty')
+let s=document.querySelector('.s')
+function aaa() {
+    document.body.style.color="red"
+    styll.style.color="pink"
+    styll.style.backgroundColor="white"
+}
+styll.addEventListener('click',aaa)
+function aa() {
+    document.body.style.color="#000"
+    sty.style.backgroundColor="#000"
+    sty.style.color="white"
+}
+sty.addEventListener('click',aa)
+function a() {
+    document.body.style.display="none"
+    s.style.color="#000"
+    s.styl.backgroundColor="white"
+}
+s.addEventListener('click',a)
+
+
