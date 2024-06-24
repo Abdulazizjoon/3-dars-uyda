0a75b0089055073eb341503e5d2397324ccf48fd
abdulaziz <abdulaziz@gmail.com>
2024-06-24 13:04:35 +0500

    ..

--- my.js
+++ my.js
@@ -1,24 +1,24 @@
-// let styll=document.querySelector('.styll')
-// let sty=document.querySelector('.sty')
-// let s=document.querySelector('.s')
-// function aaa() {
-//     document.body.style.color="red"
-//     styll.style.color="pink"
-//     styll.style.backgroundColor="white"
-// }
-// styll.addEventListener('click',aaa)
-// function aa() {
-//     document.body.style.color="#000"
-//     sty.style.backgroundColor="#000"
-//     sty.style.color="white"
-// }
-// sty.addEventListener('click',aa)
-// function a() {
-//     document.body.style.display="none"
-//     s.style.color="#000"
-//     s.styl.backgroundColor="white"
-// }
-// s.addEventListener('click',a)
+let styll=document.querySelector('.styll')
+let sty=document.querySelector('.sty')
+let s=document.querySelector('.s')
+function aaa() {
+    document.body.style.color="red"
+    styll.style.color="pink"
+    styll.style.backgroundColor="white"
+}
+styll.addEventListener('click',aaa)
+function aa() {
+    document.body.style.color="#000"
+    sty.style.backgroundColor="#000"
+    sty.style.color="white"
+}
+sty.addEventListener('click',aa)
+function a() {
+    document.body.style.display="none"
+    s.style.color="#000"
+    s.styl.backgroundColor="white"
+}
+s.addEventListener('click',a)
 
 //masalalar
 //1-masala
@@ -56,28 +56,28 @@
 
 //3-masala
 
-function a(students) {
-  let d = {};
-  students.forEach((b) => {
-    if (b.fanlari && b.fanlari.length > 0) {
-      b.fanlari.forEach((fan) => {
-        if (d[fan]) {
-          d[fan]++;
-        } else {
-          d[fan] = 1;
-        }
-      });
-    }
-  });
+// function a(students) {
+//   let d = {};
+//   students.forEach((b) => {
+//     if (b.fanlari && b.fanlari.length > 0) {
+//       b.fanlari.forEach((fan) => {
+//         if (d[fan]) {
+//           d[fan]++;
+//         } else {
+//           d[fan] = 1;
+//         }
+//       });
+//     }
+//   });
 
-  return d;
-}
+//   return d;
+// }
 
-let students = [
-  { ism: "Ali", yosh: 20, fanlari: ["Matematika", "Fizika"] },
-  { ism: "Vali", yosh: 21, fanlari: ["It-dasturlash", "Fizika"] },
-  { ism: "Sami", yosh: 22, fanlari: ["Matematika", "Kimyo", "It-dasturlash"] },
-];
+// let students = [
+//   { ism: "Ali", yosh: 20, fanlari: ["Matematika", "Fizika"] },
+//   { ism: "Vali", yosh: 21, fanlari: ["It-dasturlash", "Fizika"] },
+//   { ism: "Sami", yosh: 22, fanlari: ["Matematika", "Kimyo", "It-dasturlash"] },
+// ];
 
-let aa = a(students);
-console.log(aa);
+// let aa = a(students);
+// console.log(aa);
